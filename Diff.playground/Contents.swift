0abecf2b047d2599a13a834e3af082050326f0f3
Andrew Breckenridge <asbreckenridge@me.com>
2016-10-16 12:14:36 -0700

    create Xcodeproj

---
+++ Diff.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Diff
+
+var str = "Hello, playground"
+var other = "ello, playground"
+
+str == other
---
+++ Diff/Diff.swift
@@ -0,0 +1,13 @@
+//
+//  Diff.swift
+//  Diff
+//
+//  Created by Andrew Breckenridge on 10/16/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
+
+open class Diff {
+    
+}
